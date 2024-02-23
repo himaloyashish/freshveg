fad8922ff32bd069e76319924e4dd7c60bd514f8
himaloyash <himaloyashis98@gmail.com>
2024-02-23 10:45:30 +0600

    store is added

---
+++ src/app/store/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Store = () => {
+    return (
+        <div>
+            <p>This is Storage of all vegetables</p>
+        </div>
+    );
+};
+
+export default Store;
