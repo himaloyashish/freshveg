74fd5f6ef0c15babc4a065a4a9523da32a7d1098
himaloyash <himaloyashis98@gmail.com>
2024-02-25 00:30:32 +0600

    dynamic route created

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@
 const Home =   () => {
   return (
     <main className=" bg-[#AAD7D9] image flex min-h-screen flex-col items-center justify-between pt-4">
-      <p>This is home</p>
+      <p>This is home </p>
     </main>
   );
 }
