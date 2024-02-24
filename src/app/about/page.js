5c5415c430e45133300c9873129894ffffdafda4
himaloyash <himaloyashis98@gmail.com>
2024-02-24 19:51:24 +0600

    aout page is static

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,10 +1,11 @@
+
 import React from 'react';
 
 const About = () => {
     return (
         <div className=' px-10 bg- '>
 
-            <p>About</p>
+            <p>I'm Himaloy Ashish, the face behind FreshVeg. My love for fresh vegetables started from my childhood when my parents planted a pice of land to produce vegetable for our family need. This website isn't just about selling produce, it's about connecting people with their daily needs, seasonal Vegetable, recipes , etc.</p>
         </div>
     );
 };
