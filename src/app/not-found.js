abf88b9d19723dc5b8e7f1661dff4b05139a2ba5
himaloyash <himaloyashis98@gmail.com>
2024-02-27 12:29:35 +0600

    Now it is time to dashboard

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,10 +1,12 @@
+import Link from 'next/link';
 import React from 'react';
 
 const NotFoundPage = () => {
     return (
-        <div>
-            <p>404 !!</p>
-            <p>Mama you having a wrong way</p>
+        <div className='w-72 m-auto p-6 text-center'>
+            <p className='text-red-500 text-4xl'>404 !!</p>
+            <p className='text-xl'>You having a wrong way</p>
+            <Link href={'/'} className='text-xl bg-green-300 rounded-sm p-3'>Go Back</Link>
         </div>
     );
 };
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,4 +1,15 @@
+"use client"
+import { IconButton } from '@mui/material';
+import Link from 'next/link';
 import React from 'react';
+
+// // MUI icon 
+// import AddLocationIcon from '@mui/icons-material/AddLocation';
+// import LanguageIcon from '@mui/icons-material/Language';
+// import CallEndIcon from '@mui/icons-material/CallEnd';
+// import FacebookIcon from '@mui/icons-material/Facebook';
+// import InstagramIcon from '@mui/icons-material/Instagram';
+// import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 const Footer = () => {
     return (
@@ -6,7 +17,63 @@ const Footer = () => {
             <p>FreshVeg</p>
 
             <div>
-                
+                <div>
+                    <div>
+                        <p>
+                            Welcome to FreshVeg, your online haven for the freshest, most vibrant vegetables you can find! Whether you're a seasoned home cook, a busy professional seeking healthy options, or simply someone looking to add more color to your plate, FreshVeg has everything you need.
+                        </p>
+                    </div>
+                    <div>
+                        <input type="email" name="" id="" />
+                        <button type="submit">Submit</button>
+                    </div>
+                    <div>
+                        <p>Information</p>
+                        <div>
+                            <Link href={'about'}>About Us</Link>
+                            <Link href={'contact'}>Contact Us</Link>
+                            <Link href={'contact'}>Terms & Condition</Link>
+                            <Link href={'blog'}>Return & Refunds</Link>
+                            <Link href={'blog'}>Blog</Link>
+                        </div>
+                    </div>
+                    <div>
+                        <p>Quick Contact</p>
+                        <div>
+                            <div className='flex'>
+                                <IconButton aria-label="delete">
+                                    <AddLocationIcon />
+                                </IconButton>
+                                <p>Gulshan 2, Round Square</p>
+                            </div>
+                            <div className='flex'>
+                                <IconButton aria-label="delete">
+                                    <LanguageIcon />
+                                </IconButton>
+                                <p>https://freshveg-cyan.vercel.app/</p>
+                            </div>
+                            <div className='flex'>
+                                <IconButton aria-label="delete">
+                                    <CallEndIcon />
+                                </IconButton>
+                                <p>+88 99878 - 8779</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <p>Information</p>
+                        <IconButton aria-label="FacebookIcon">
+                            <FacebookIcon />
+                        </IconButton>
+                        <IconButton aria-label="InstagramIcon">
+                            <InstagramIcon />
+                        </IconButton>
+                        <IconButton aria-label="LinkedInIcon">
+                            <LinkedInIcon />
+                        </IconButton>
+                    </div>
+
+                </div>
             </div>
         </div>
     );
