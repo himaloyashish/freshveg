873a0a5f33186e53020d8d909a96bc3d21cacc25
himaloyash <himaloyashis98@gmail.com>
2024-03-07 00:00:38 +0600

    dashboard

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -62,7 +62,7 @@ const DrawerHeader = styled('div')(({ theme }) => ({
   display: 'flex',
   alignItems: 'center',
   padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
+  // necessary for content to  be below app bar
   ...theme.mixins.toolbar,
   justifyContent: 'flex-end',
 }));
